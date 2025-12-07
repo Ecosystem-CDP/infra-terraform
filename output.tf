d951e23c310429fa7a2ccd0225b872dc67613c35
João Pedro Ferreira Duarte <joaopedrofduarte@outlook.com>
2025-12-06 23:03:36 -0300

    fix: try to verify hosts

--- output.tf
+++ output.tf
@@ -4,5 +4,5 @@ output "generated_private_key_pem" {
 }
 
 output "comments" {
-  value = "The instalation of ambari will be finishied in about 10 minutes. The URL for ambari is ${oci_core_instance.Master.public_ip}:8080. Try ssh to Master in case application does not load."
+  value = "The instalation of ambari will be finishied in about 10 minutes, and then there will be the instalation of the cluster. The URL for ambari is ${oci_core_instance.Master.public_ip}:8080. Try ssh to Master in case application does not load."
 }
